7cc66660ea67919c74db7703744eec98d88dc226
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-09-03 15:50:28 -0400

    Add initial project files for TTS-COMPONENTS Edu Edition GA 2nd

---
+++ TTS-COMPONENTS Edu Edition GA 2nd/TTS-COMPONENTS Edu Edition GA 2nd/Form1.cs
@@ -0,0 +1,10 @@
+namespace TTS_COMPONENTS_Edu_Edition_GA_2nd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
