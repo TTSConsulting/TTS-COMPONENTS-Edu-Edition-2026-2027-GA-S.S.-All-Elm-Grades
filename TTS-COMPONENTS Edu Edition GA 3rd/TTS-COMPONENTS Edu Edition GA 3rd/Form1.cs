4c6adef0639de984136712dd5878158e442bc0f9
Coolis1362 <tadeotherocketbuilder@outlook.com>
2025-09-05 21:15:55 -0400

    feat: Add initial project files for TTS-COMPONENTS Edu Edition GA 3rd

---
+++ TTS-COMPONENTS Edu Edition GA 3rd/TTS-COMPONENTS Edu Edition GA 3rd/Form1.cs
@@ -0,0 +1,10 @@
+namespace TTS_COMPONENTS_Edu_Edition_GA_3rd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
